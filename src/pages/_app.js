0a5ca89ce8e5f0954b4de692c9fb1a12a7c9685f
Scott Maitland <scott@maitland.me.uk>
2023-03-16 22:20:30 +0000

    sass styling and route creation
    Generated the sass 7-in-1 folder and file structure along with creating the inital pages/routes for the application to use

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import "@/sass/main.css";
+import Layout from "../Layout/layout.js";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
--- src/pages/settingsScreen.js
+++ src/pages/settingsScreen.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
 
-export default function index() {
+export default function settingsScreen() {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta name="description" content="Landing page for Tasker" />
+        <title>Tasker | Settings</title>
+        <meta name="description" content="Settings page for Tasker" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Home Screen</h1>
+      <h1>Settings Screen</h1>
     </>
   );
 }
