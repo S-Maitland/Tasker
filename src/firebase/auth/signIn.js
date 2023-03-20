9d31ec5a015223f7b4c4e6f4e36bd5de6393a104
Scott Maitland <scott@maitland.me.uk>
2023-03-20 23:28:05 +0000

    firebase init

---
+++ src/firebase/auth/signIn.js
@@ -0,0 +1,17 @@
+import firebase_app from "../config";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(firebase_app);
+
+export default async function signIn(email, password) {
+  let result = null;
+  let error = null;
+
+  try {
+    result = await signInWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
